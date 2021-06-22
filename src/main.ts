bdcfe288736413303d0b4cd2f426e78cc15ccbd9
skarbgud <54926902+skarbgud@users.noreply.github.com>
2021-06-22 09:37:00 +0900

    Create Angular

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,17 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
+// import { BOOMR } from 'boomerangjs';
+
+// BOOMR.init({
+//   beacon_url: "http://http://192.168.121.164:4200/beacon",
+//   angular: true,
+  
+// });
+
+declare global {
+  interface Window { strum: Function; }
+}
 
 if (environment.production) {
   enableProdMode();
